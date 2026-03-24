8ae7d3bad6c4b5059b0365b8185684eaa50c0d72
geunseonkim <guensoen@gmail.com>
2026-03-24 18:34:37 +0900

    feat: 에러 처리 개선

--- bin/redichat.js
+++ bin/redichat.js
@@ -1,2 +1,16 @@
 #!/usr/bin/env node
-await import("../dist/ui.js");
+try {
+  await import("../dist/ui.js");
+} catch (error) {
+  if (error.code === "ERR_MODULE_NOT_FOUND") {
+    console.error(
+      "\x1b[31mError: The application has not been built yet.\x1b[0m",
+    );
+    console.error(
+      'Please run \x1b[32m"npm run build"\x1b[0m before starting the application.',
+    );
+    process.exit(1);
+  }
+  console.error("\x1b[31mAn unexpected error occurred:\x1b[0m", error);
+  process.exit(1);
+}
